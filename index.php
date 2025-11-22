334111e2df417504c3b49ba806b0963fd7187b00
nanangkus <nanangkusnandar047@gmail.com>
2025-11-22 12:33:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$url = 'ISI DOMAIN RAW';
+$url = 'https://raw.githubusercontent.com/nanangkus/netlify/refs/heads/main/file.txt';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
